71fbba04679fc7acd650472265c5a5cd69ac57cf
= <faainamacaldera16@gmail.com>
2023-04-05 13:19:55 +0700

    :heavy_plus_sign: :feat(Strapi)Add Strapi dashboard, and create sum of Collection Type (Page, DMM, RPL, TKJ)

---
+++ api/src/api/dmm/controllers/dmm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dmm controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dmm.dmm');
---
+++ api/src/api/rpl/routes/rpl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rpl router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rpl.rpl');
